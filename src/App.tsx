b3d0459f952c2bb12669b16c2282081dd525a0ca
Semen Bushmanov <bush.sem@mail.ru>
2025-11-08 20:34:40 +0500

    Create base for weather forecast app (React, Tailwind, Vite)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-sky-200 text-center">
+        <h1 className="text-5xl font-bold text-blue-700 mb-6">Weather App</h1>
+        <p className="text-gray-700">Weather forecast ☀️</p>
+      </div>
+    </>
+  )
+}
+
+export default App
